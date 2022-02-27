b75efb7b3771bd5cf295c7d697f80756fd84799c
nrimac <nrimac00@gmail.com>
2022-02-27 15:30:16 +0100

    Added timer to sudoku

---
+++ GameSite/src/timer.js
@@ -0,0 +1,14 @@
+const startingMinutes = 20;
+let time = startingMinutes * 60;
+const timer = document.getElementById("timer");
+
+function updateCountdown() {
+  const minutes = Math.floor(time / 60);
+  let seconds = time % 60;
+
+  seconds = seconds < 10 ? "0" + seconds : seconds;
+
+  timer.innerHTML = `${minutes}:${seconds}`;
+
+  time--;
+}
