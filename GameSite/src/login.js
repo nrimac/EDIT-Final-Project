e49d3053a0de0511a3294b9e3458b7bbb2343d0d
nrimac <nrimac00@gmail.com>
2022-02-28 22:24:40 +0100

    Added login function to index.html

--- GameSite/src/login.js
+++ GameSite/src/login.js
@@ -9,5 +9,60 @@ const firebaseConfig = {
 
 const app = firebase.initializeApp(firebaseConfig);
 
-const db = app.firestore();
+const database = app.firestore();
 
+const btnLogin = document.getElementById("btn-login");
+const btnRegister = document.getElementById("btn-register");
+const form = document.getElementsByTagName("form")[0];
+const name = document.getElementById("username");
+const password = document.getElementById("password");
+const errorElement = document.getElementById("error");
+
+btnLogin.addEventListener("click", login);
+
+btnRegister.addEventListener("click", register);
+
+form.addEventListener("submit", (e) => {
+  let messages = [];
+  if (name.value === "" || name.value === null) {
+    messages.push("Name is requred");
+  }
+
+  if (password.value.length < 6) {
+    messages.push("Password must be longer than 5 characters!");
+  }
+
+  if (password.value.length >= 20) {
+    messages.push("Password mustn't be longer than 20 characters!");
+  }
+
+  if (messages.length > 0) {
+    errorElement.innerText = messages.join(", ");
+  } else {
+    errorElement.innerText = "";
+  }
+
+  e.preventDefault();
+});
+
+async function login() {
+  let user = null;
+
+  await database
+    .collection("Users")
+    .where("username", "==", `${name.value}`)
+    .get()
+    .then((querySnapshot) =>
+      querySnapshot.forEach((doc) => {
+        user = doc.data();
+      })
+    );
+
+  if (user != null && user.password === password.value) {
+    console.log(user);
+  } else {
+    console.log("nah");
+  }
+}
+
+async function register() {}
